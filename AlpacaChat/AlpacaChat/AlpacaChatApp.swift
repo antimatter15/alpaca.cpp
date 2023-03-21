a2aee4656a2f70ae4f6f008b015d143a5bdabd13
Yi Wang <yi.wang.2005@gmail.com>
2023-03-20 20:04:47 -0700

    Add an iOS app

---
+++ AlpacaChat/AlpacaChat/AlpacaChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlpacaChatApp.swift
+//  AlpacaChat
+//
+//  Created by Yi Wang on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AlpacaChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
